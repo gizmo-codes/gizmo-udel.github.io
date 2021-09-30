4d8ac38b8b76b564555379d4212bcd326cf7b70a
Matt Chandler <Omzig7@gmail.com>
2021-09-30 12:09:14 -0400

    further improved UX/UI - removed instances of alert() and replaced with a popup box close to the position of the error or success for user feedback

--- js/app.js
+++ js/app.js
@@ -8,6 +8,20 @@ window.onclick = function (event) {
     }
 }
 
+// Close operations for the alert box
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function () {
+            div.style.display = "none";
+        }, 600);
+    }
+}
+
 // Phone number formatting (123-456-7890)
 function phoneCheck(phoneno) {
     var numbers = /^\d{3}-\d{3}-\d{4}$/;
@@ -38,23 +52,23 @@ function emailCheck(input) {
     }
 }
 
-function clearfname(){
+function clearfname() {
     document.getElementById("user_fname").style.borderColor = "";
 }
 
-function clearlname(){
+function clearlname() {
     document.getElementById("user_lname").style.borderColor = "";
 }
 
-function clearemail(){
+function clearemail() {
     document.getElementById("user_email").style.borderColor = "";
 }
 
-function clearphone(){
+function clearphone() {
     document.getElementById("user_phone").style.borderColor = "";
 }
 
-function clearmessage(){
+function clearmessage() {
     document.getElementById("user_message").style.borderColor = "";
 }
 
@@ -64,6 +78,7 @@ function clearmessage(){
 
 // TODO: Implement logic so that ALL fields are checked all at once, instead of the current implementation of top to bottom.
 // TODO: Implement a better error display than alert(), nothing I have tried worked :( 
+// TODO: Look into style.display showing the alert when an error occurs
 function validate() {
     let submit = document.querySelector(".submit")
     submit.addEventListener("click", (e) => {
@@ -82,12 +97,11 @@ function validate() {
         //Checking for appropriate input.
 
         //Checking First Name for empty field
-        if (user_fname.value.length == 0)
-        { 
+        if (user_fname.value.length == 0) {
             document.getElementById("user_fname").style.borderColor = "red";
-            document.getElementById("user_fname").placeholder = "Field required!";	
-           return false; 
-        } 
+            document.getElementById("user_fname").placeholder = "Field required!";
+            return false;
+        }
         // Alpha Character only validation for name, not needed for specs but working!
         /*
         if (textCheck(user_fname) == false){
@@ -98,11 +112,10 @@ function validate() {
         */
 
         // Checking Last Name for empty field.
-        if (user_lname.value.length == 0)
-        { 
+        if (user_lname.value.length == 0) {
             document.getElementById("user_lname").style.borderColor = "red";
-            document.getElementById("user_lname").placeholder = "Field required!"; 	
-           return false; 
+            document.getElementById("user_lname").placeholder = "Field required!";
+            return false;
         }
         // Alpha Character only validation for name, not needed for specs but working!
         /*
@@ -114,46 +127,53 @@ function validate() {
         */
 
         // Checking eMail for empty field.
-        if (user_email.value.length == 0)
-        { 
+        if (user_email.value.length == 0) {
             document.getElementById("user_email").style.borderColor = "red";
-            document.getElementById("user_email").placeholder = "Field required!";	
-           return false; 
+            document.getElementById("user_email").placeholder = "Field required!";
+            return false;
         }
         // Checking eMail for correct formatting.
         if (emailCheck(user_email) == false) {
             document.getElementById("user_email").style.borderColor = "red";
-            alert("You have entered an invalid email address.\nFormat example: xyz@domain.com");
+
+            // Needed to redisplay the alert if the user closes it
+            document.getElementById("alert").attributeStyleMap.clear();
+
+            document.getElementById("alertmessage").innerHTML = "You have entered an invalid email address.\nFormat example: xyz@domain.com";
+            document.getElementById("alert").style.display = null;
             return false;
         }
 
         // Checking Phone Number for empty field.
-        if (user_phone.value.length == 0)
-        { 
+        if (user_phone.value.length == 0) {
             document.getElementById("user_phone").style.borderColor = "red";
-            document.getElementById("user_phone").placeholder = "Field required!";	
-           return false; 
+            document.getElementById("user_phone").placeholder = "Field required!";
+            return false;
         }
         // Checking phone number for 123-456-7890 format.
         if (phoneCheck(user_phone.value) == false) {
             document.getElementById("user_phone").style.borderColor = "red";
-            alert("Incorrect format: 123-456-7890");
+
+            // Needed to redisplay the alert if the user closes it
+            document.getElementById("alert").attributeStyleMap.clear();
+
+            document.getElementById("alertmessage").innerHTML = "Please use the following format: 123-456-7890";
+            document.getElementById("alert").style.display = null;
             return false;
         }
 
         // Checking message for empty field.
-        if (user_message.value.length == 0)
-        { 
+        if (user_message.value.length == 0) {
             document.getElementById("user_message").style.borderColor = "red";
-            document.getElementById("user_message").placeholder = "Field required!"; 	
-           return false; 
+            document.getElementById("user_message").placeholder = "Field required!";
+            return false;
         }
 
         // If all of the above pass, call the sendMail function and close the modal.
         else {
             sendMail(full_name, user_email, user_phone, user_message);
             modal.style.display = "none";
-            alert("Successfully sent!")
+            document.getElementById("alertsuccess").style.display = null;
         }
     })
 }
